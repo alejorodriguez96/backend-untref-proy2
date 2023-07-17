45fdbe26b94b939a1d345f1b48c3281491e2a4ea
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2023-07-17 02:17:42 -0300

    agregados enpoints get

--- server.js
+++ server.js
@@ -16,6 +16,63 @@ app.get("/", (req, res) => {
   res.status(200).end("Bienvenido a la API de Computación");
 });
 
+// Ruta para obtener todos los documentos de la colección
+app.get("/productos", async (req, res) => {
+    const collection = await getCollection("productos");
+    const productos = await collection.find({}).toArray();
+    res.status(200).json(productos);
+});
+
+// Ruta para obtener un documento de la colección por su id
+app.get("/productos/:id", async (req, res) => {
+    try {
+        const collection = await getCollection("productos");
+        const producto = await collection.findOne({ codigo: parseInt(req.params.id) });
+        console.log(producto);
+        if (!producto) {
+            res.status(404).json({ error: "Producto no encontrado" });
+        } else {
+            res.status(200).json(producto);
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error al conectarse a la BBDD" });
+    }
+});
+
+// Ruta para obtener un documento de la colección por su nombre o parte de él
+app.get("/productos/nombre/:nombre", async (req, res) => {
+    try {
+        const collection = await getCollection("productos");
+        const producto = await collection.find({ nombre: { $regex: req.params.nombre, $options : 'i' } }).toArray();
+        if (!producto) {
+            res.status(404).json({ error: "Producto no encontrado" });
+        } else {
+            res.status(200).json(producto);
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error al conectarse a la BBDD" });
+    }
+});
+
+// Ruta para obtener un documento de la colección por su categoría o parte de ella
+app.get("/productos/categoria/:categoria", async (req, res) => {
+    try {
+        const collection = await getCollection("productos");
+        const producto = await collection.find({ categoria: { $regex: req.params.categoria, $options : 'i' } }).toArray();
+        if (!producto) {
+            res.status(404).json({ error: "Producto no encontrado" });
+        } else {
+            res.status(200).json(producto);
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Error al conectarse a la BBDD" });
+    }
+});
+
+app.get("*", (req, res) => {
+    res.status(404).end("URL no encontrada");
+});
+
 
 // Iniciar el servidor
 app.listen(PORT, () => {
